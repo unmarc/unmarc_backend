75bc5011676b2a092eb23a4dee4ba58bc6860425
Charles <charlesj@gmail.com>
2019-11-05 23:45:42 +0530

    Remove cruft from UserModelTest

--- unmarc/users/tests.py
+++ unmarc/users/tests.py
@@ -1,17 +1,13 @@
 from django.test import TestCase
 
 from users.models import User, Staff
-from library.models import Branch, Library
 
 
 class UserModelTest(TestCase):
     def setUp(self):
         self.regular_user = User.objects.create_user('Rocky Balboa')
         self.staff_user = User.objects.create_user('Frank Slade')
-        _library = Library.objects.create(name='Buguri', short_id='buguri')
-        _branch = Branch.objects.create(name='JP Nagar', library=_library, short_id='jp-nagar')
-        _staff = Staff.objects.create(user=self.staff_user)
-        _staff.branches.add(_branch)
+        Staff.objects.create(user=self.staff_user)
 
     def test_non_staff_user_has_is_library_staff_property_false(self):
         self.assertFalse(self.regular_user.is_library_staff)
